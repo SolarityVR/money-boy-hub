296a1c599bea287c3f5e79c9d73c0ac2b418e04b
pietro <pammaturo02@gmailcom>
2022-03-29 21:37:49 +0200

    added dinamically generated nfts (in frames)

---
+++ room/scripts/custom/frames.js
@@ -0,0 +1,18 @@
+var frameEl_array = document.getElementsByClassName("frame");
+console.log(frameEl_array)
+var frame_array = [{ "image": "assets/images/sky.jpg" }, { "image": "assets/images/sky.jpg" }, { "image": "assets/images/sky.jpg" }, { "image": "assets/images/sky.jpg" }, { "image": "assets/images/sky.jpg" }]
+function build_frames() {
+    var frame_count = 0;
+    for (let frameEl of frameEl_array) {
+        var frame_imageEL = document.createElement('a-image');
+        frameEl.appendChild(frame_imageEL);
+        frame_imageEL.setAttribute('src', frame_array[frame_count].image);
+        frame_imageEL.setAttribute("width", 1.1);
+        frame_imageEL.setAttribute("height", 1.1);
+        frame_imageEL.setAttribute('position', { x: 0, y: 0, z: 0.01 });
+        frame_count++;
+    }
+}
+function start_frames(){
+    build_frames();
+}
