82b83496d09c5155c74d26ad746ada29681bf35b
pietro <pammaturo02@gmailcom>
2022-04-01 13:09:37 +0200

    added floor price in room

--- room/scripts/custom/screen.js
+++ room/scripts/custom/screen.js
@@ -1,15 +1,48 @@
-var gif_img_index = 0;
-var gif_img_array = ["#gif-img1", "#gif-img2", "#gif-img3", "#gif-img4"]
-function startGif() {
-    document.getElementById("screen_img").setAttribute("src", gif_img_array[gif_img_index]);
-    if (gif_img_index < gif_img_array.length - 1)
-        gif_img_index++;
-    else
-        gif_img_index = 0;
+const urlBase = "https://solarity.muhash.com/api";
+const apiCaller = axios.create({
+    baseURL: urlBase,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+//nft
+var nft_containerEl = document.getElementById('screen');
+var nft;
+apiCaller
+    .get("/test/collections/solana_money_boy")
+    .then((data) => {
+        nft = data.data.collection;
+    })
+    .catch((err) => {
+        nft = { "floorPrice": "no data", "image": "assets/images/nft_placeholder.jpeg" };
+    });
+function build_nft() {
+
+    var nft_item_amountEL = document.createElement('a-text');
+    nft_containerEl.appendChild(nft_item_amountEL);
+    nft_item_amountEL.setAttribute('value', nft.floorPrice);
+    nft_item_amountEL.setAttribute('wrap-count', 15);
+    nft_item_amountEL.setAttribute('baseline', "top");
+    nft_item_amountEL.setAttribute('x-offset', 0.05);
+    nft_item_amountEL.setAttribute("width", 0.9);
+    nft_item_amountEL.setAttribute("align", "center");
+    nft_item_amountEL.setAttribute('position', { x: 0.5, y: 0.1, z: 0.01 });
+    nft_item_amountEL.setAttribute("color", "#AAEEFF");
+
+
+    var nft_item_imageEL = document.createElement('a-image');
+    nft_containerEl.appendChild(nft_item_imageEL);
+    nft_item_imageEL.setAttribute('src', nft.image);
+    nft_item_imageEL.setAttribute("width", 1.1);
+    nft_item_imageEL.setAttribute("height", 1.1);
+    nft_item_imageEL.setAttribute('position', { x: -0.4, y: 0, z: 0.01 });
+    build_nft_listeners();
+}
+
+function build_nft_listeners() {
+
 }
 
 function start_screen() {
-    var intervalId = window.setInterval(function () {
-        startGif();
-    }, 500);
+    build_nft();
 }
