5d73bd2af93bebd3d0e6dde450d82ce4cf09eb47
Thomas Meta <thomerda@gmail.com>
2022-04-02 14:33:42 -0700

    Update frames.js

--- room/scripts/custom/frames.js
+++ room/scripts/custom/frames.js
@@ -6,8 +6,8 @@ function build_frames() {
         var frame_imageEL = document.createElement('a-image');
         frameEl.appendChild(frame_imageEL);
         frame_imageEL.setAttribute('src', frame_array[frame_count].image);
-        frame_imageEL.setAttribute("width", 1.1);
-        frame_imageEL.setAttribute("height", 1.1);
+        frame_imageEL.setAttribute("width", 0);
+        frame_imageEL.setAttribute("height", 0);
         frame_imageEL.setAttribute('position', { x: 0, y: 0, z: 0.01 });
         frame_count++;
     }
